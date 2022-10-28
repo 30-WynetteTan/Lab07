3bb2f8d7cf797c65d10dcee2ba29a0f0ae2029b3
30-WynetteTan <114891331+30-WynetteTan@users.noreply.github.com>
2022-10-28 15:16:01 +0800

    Completed Lab07

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 //This script manages the behavior of individual obstacle
 public class Obstacle : MonoBehaviour
 {
-    [SerializeField] private float Speed = 3;
+    [SerializeField] private float xSpeed = 3f;
+    public static int score;
 
     void Update()
     {
+        //transform.Translate(new Vector3(xSpeed * Time.deltaTime, 0, 0f));
         if (transform.position.x <= -8)
-            Destroy(gameObject);
+        {
+            Destroy(this.gameObject);
+            score++;
+        }
         else
-            transform.Translate(Vector3.right * Time.deltaTime * -Speed);
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * -xSpeed);
+        }            
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public Rigidbody rb;
+    public float speed = 2.5f;
+
+    private int Score;
+    public Text scoretext;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rb.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        Score = Obstacle.score;
+        scoretext.text = "Score: " + Score;      
+
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
             thisAnimation.Play();
+            rb.velocity = Vector2.up * speed;
+        }
+        if (transform.position.y <= -4)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }      
     }
 }
